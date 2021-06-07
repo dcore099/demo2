6289bca963ca24dff45bf98d59c9c6ddf3ba1f16
Daniel <dcore099@gmail.com>
2021-06-06 19:52:27 -0500

    bla

---
+++ src/main/java/com/beans/Greeting.java
@@ -0,0 +1,26 @@
+package com.beans;
+/*
+ * Represents a greeting duh!
+ */
+
+public class Greeting {
+	
+	public long id;
+	public String content;	
+	
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public long getId() {
+		return id;
+	}
+	public String getContent() {
+		return content;
+	}	
+	
+
+}
---
+++ src/main/java/com/rest/controllers/GreetingController.java
@@ -0,0 +1,5 @@
+package com.rest.controllers;
+
+public class GreetingController {
+
+}
