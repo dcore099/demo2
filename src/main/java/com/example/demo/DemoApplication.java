0d95d9d6f26fa132a42b414a86a83f09bbe1a9cb
Daniel <dcore099@gmail.com>
2021-06-06 19:36:43 -0500

    commit2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		//	yoooo
+		//	yoooo yoooo
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
